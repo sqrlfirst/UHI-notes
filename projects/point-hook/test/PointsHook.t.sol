82c9004d5292ea9b5af2b4a75a7446159ec8c948
Egor Dergunov <sqrlfirst@Egors-MacBook-Pro.local>
2024-04-21 22:29:42 +0700

    started tests

--- projects/point-hook/test/PointsHook.t.sol
+++ projects/point-hook/test/PointsHook.t.sol
@@ -29,6 +29,46 @@ contract TestPointsHook is Test, Deployers {
     PointsHook hook;
 
     function setUp() public {
-        
+        // Deploy PoolManager and Router contracts
+        deployFreshManagerAndRouters();
+
+        // Deploy out Token Contract
+        token = new MockERC20("Test Token", "TEST", 18);
+        tokenCurrency = Currency.wrap(address(token));
+
+        // 
+        token.mint(address(this), 1000 ether);
+        token.mint(address(1), 1000 ether);
+
+        uint160 flags = uint160(Hooks.AFTER_ADD_LIQUIDITY_FLAG | Hooks.AFTER_SWAP_FLAG);
+        (, bytes32 salt) = HookMiner.find(
+            address(this),
+            flags,
+            0,
+            type(PointsHook).creationCode,
+            abi.encode(manager, "Points Token", "TEST_POINTS");
+        );
+
+        hook = new PointsHook{salt: salt}(
+            manager, 
+            "Points Token",
+            "TEST_POINTS"
+        );
+
+        token.approve(address(swapRouter), type(uint256).max);
+        token.approve(address(modifyLiquidityRouter), type(uint256).max);
+    
+        (key, ) = initPool(
+            ethCurrency, // Currency 0 - ETH
+            tokenCurrency, // Currency 1 - TOKEN
+            hook, // HookContract
+            3000, // SwapFees
+            SQRT_RATIO_1_1, // Initial SQRT(P) value = 1
+            ZERO_BYTES // No additional `initData`
+        );
     }
+
+    function test_addLiquidityAndSwap() public {}
+
+    function test_addLiquidityAndSwapWithReferral() public {}
 }
